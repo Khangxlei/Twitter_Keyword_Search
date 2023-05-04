dad2c2f1fa5cc8f12a4dbcbf7afa6aac45474513
Khang Le <98366987+Khangxlei@users.noreply.github.com>
2023-05-03 23:32:27 -0400

    Updated main.cpp to output into a file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include<fstream>
 #include<sstream>
 #include <unordered_set>
+#include <algorithm>
 
 #include "inverted_index.h"
 
@@ -36,16 +37,22 @@ int main(int argc, char*argv[]) {
 
   InvertedIndex Data;
 
+
   // user inputs
   string input;
   cout << "Please enter input file name: ";
   getline(cin, input);
   Data.addfile(input);
 
+  string outputfile_name = "output_" + input;
+  ofstream outfile;
+  outfile.open(outputfile_name);
+  
   vector<string> input_words;
   
   cout << "Please enter search: ";
   getline(cin, input);
+  transform(input.begin(), input.end(), input.begin(), ::tolower);
 
   // parses the keywords that the user inputs into vectors of words
   input_words = parse_input(input);
@@ -57,11 +64,11 @@ int main(int argc, char*argv[]) {
                                                   // and the other for the tweet number: tweet. 
   }
 
-  cout << "\nTweet IDs that contains at least one word in the given query" << endl;
+  outfile << "\nTweet IDs that contains at least one word in the given query" << endl;
   for ( unordered_set<int>::iterator it = tweets_set.begin(); it != tweets_set.end(); ++it){
-    cout << *it << ' ';
+    outfile << *it << ' ';
   }
-  cout << "\n\n";
+  outfile << "\n\n";
 
   // add everything from the set back into the vector for easy iteration and data access
   vector<int> tweets_non_dup;
@@ -70,22 +77,22 @@ int main(int argc, char*argv[]) {
     tweets_non_dup.push_back(*it);
   }
 
-  cout << "\nTweet IDs associated with their similarity scores" << endl;
+  outfile << "\nTweet IDs associated with their similarity scores" << endl;
   for (unsigned int i=0; i<tweets_non_dup.size(); ++i){
-    cout << "ID: " << tweets_non_dup[i] << "  Similarity Score: " << Data.similarity_score[tweets_non_dup[i]] << endl;
+    outfile << "ID: " << tweets_non_dup[i] << "  Similarity Score: " << Data.similarity_score[tweets_non_dup[i]] << endl;
   }
-  cout << "\n\n";
+  outfile << "\n\n";
 
   vector<int> relevant; // hold all the tweets in ascending order of similarity score
   relevant = Data.relevant_tweets(tweets_non_dup); // calculates and sorts the tweets in ascending order in terms of similarity score
 
   // prints out top 10 similar tweets
-  cout << "Printing Top 10 Similar Tweets:" << endl;
+  outfile << "Printing Top 10 Similar Tweets:" << endl;
   int i = relevant.size()-1;
   unsigned int count = 0;
   while (i >= 0 && count < 10){
-    cout << count +1 << ". ";
-    cout << Data.tweet[relevant[i]] << endl;
+    outfile << count +1 << ". ";
+    outfile << Data.tweet[relevant[i]] << endl;
     count++;
     i--;
   }
